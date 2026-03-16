770fce6a5e37d83074261a3d022f5c8d16d96267
yakshitabrindhalakshmi <brindhalakshmiyakshita@gmail.com>
2026-03-16 10:52:34 +0530

    Added UseCase3 Palindrome Checker App

---
+++ UseCase3PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+
+    }
+}
