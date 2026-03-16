460bedc8597befde1dc931704d8bde96dcb8539f
yakshitabrindhalakshmi <brindhalakshmiyakshita@gmail.com>
2026-03-16 11:12:04 +0530

    Added UseCase7 Palindrome Checker App

---
+++ UseCase8PalindromeCheckerApp.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+public class UseCase8PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            if (list.removeFirst() != list.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+
+    }
+}
