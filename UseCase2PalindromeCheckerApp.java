4b65fd994334ddb337d48dd84657fe22600d6f5d
yakshitabrindhalakshmi <brindhalakshmiyakshita@gmail.com>
2026-03-16 10:50:30 +0530

    Added UseCase2 Palindrome Checker App

---
+++ UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
